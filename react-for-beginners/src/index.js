91f22688bb42f2e3a38342b939f3167741abf17a
Loop <shwlsdn0906@gmail.com>
2022-01-24 23:47:43 +0900

    MovieApp을 위한React Router 설치 및 components, router폴더 제작

--- react-for-beginners/src/index.js
+++ react-for-beginners/src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import CoinTrackerApp from "./CoinTrackerApp";
+import MovieApp from "./MovieApp";
 import ToDoApp from "./ToDoApp";
 
 ReactDOM.render(
     <React.StrictMode>
-        <ToDoApp />
+        {/* <ToDoApp />
         <CoinTrackerApp />
+        <hr /> */}
+        <MovieApp />
     </React.StrictMode>,
     document.getElementById("root")
 );
